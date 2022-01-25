101b022a57a352d010a1d587c9ac76ac996d0832
Martin Scheiber <martin.scheiber@aau.at>
2022-01-25 08:43:53 +0100

    [FIX] Bugfix for stopping recording script

--- scripts/stop_record.sh
+++ scripts/stop_record.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-rosnode list | grep "/record_" | xargs rosnode kill
+NODES=$(rosnode list | grep "/record_")
+
+if [[ ! -z ${NODES} ]]; then
+  echo ${NODES} | xargs rosnode kill
+else
+  echo "[RECORD_STOP] already all recordings stop -- skipping redundant killing"
+fi
